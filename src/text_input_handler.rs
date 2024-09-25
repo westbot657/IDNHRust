6744362678f13f36975d7ab9d5dce8a9f7f37dba
Weston Day <82739021+westbot657@users.noreply.github.com>
2024-09-24 19:33:19 -0600

    idk

--- src/text_input_handler.rs
+++ src/text_input_handler.rs
@@ -1,12 +1,22 @@
 
 
 // Using this ensures that all text edit indexing uses the same type
-type IdxSize = u64;
+type IdxSize = usize;
 
 pub struct Selection {
     start_index: IdxSize,
     end_index: IdxSize,
     slice: String
+}
+
+impl Clone for Selection {
+    fn clone(&self) -> Self {
+        Self {
+            start_index: self.start_index,
+            end_index: self.end_index,
+            slice: self.slice.clone()
+        }
+    }
 }
 
 impl Selection {
@@ -52,19 +62,44 @@ impl TextInputHandler {
         }
     }
 
+    /// Removes additional cursors and deselects all text, moves the cursor to the specified position (clamped to the length of the text)
+    pub fn set_cursor_index(&mut self, idx: IdxSize) {
+        self.secondary_cursors.clear();
+        self.cursor_idx = idx.min(self.content.len() as IdxSize);
+
+    }
+
+    /// Used for typing, automatically accounts for selected text and multiple cursors
     pub fn insert_at_cursor(&mut self, content: String) {
 
     }
 
+    /// pastes content. Follows a variety of rules for pasting based
+    /// off what is being pasted and how many cursors exist at the time
+    pub fn paste_at_cursor(&mut self) {
+
+    }
+
+    /// does a backspace, accounting for selected text and multiple cursors
+    pub fn backspace_at_cursor(&mut self) {
+        let mut indexes = self.secondary_cursors.clone();
+        indexes.push(self.cursor_idx);
+        indexes.sort();
+
+        // thing to account for shifting since we're removing content, which de-syncs the indices
+        let mut idx_mod = 0;
+
+
+    }
+
+    /// same as backspace_at_cursor, but with delete behavior
     pub fn delete_at_cursor(&mut self) {
 
     }
 
+    /// Returns a Vec of all Selections. This Vec will be empty if no text is selected.
     pub fn get_selections(&self) -> Vec<Selection> {
-        let mut out = Vec::new();
-
-
-        out
+        self.selections.clone()
     }
 
 
